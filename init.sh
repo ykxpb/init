ed5cbc6613912624ddfeb9460e01a4c2b974d0b7
Yuan Yang <cyberyuanyang@gmail.com>
2016-03-11 07:28:06 +0000

    update gitconfig

--- init.sh
+++ init.sh
@@ -16,35 +16,40 @@ cp gitconfig ~/.gitconfig
 cp gitignore_global ~/.gitignore_global
 cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
-cp zshrc ~/.zshrc
+#cp zshrc ~/.zshrc
 
 # utils
-pip install --user git+git://github.com/Lokaltog/powerline
-pip install --user flake8
+
+# TODO powerline not work
+#pip install --user git+git://github.com/Lokaltog/powerline
+#ln -s {path_to_powerline}/scripts/powerline ~/.local/bin
+#cp /home/vagrant/.local/lib/python2.7/site-packages/powerline/bindings/vim/plugin
+#cp -r {path_to_powerline}/bindings/vim/plugin ~/.vim/
+
+# for python
+#pip install --user flake8
 pip install --user ipython
 
 mkdir -p ~/.local/bin
-ln -s {path_to_powerline}/scripts/powerline ~/.local/bin
-#cp /home/vagrant/.local/lib/python2.7/site-packages/powerline/bindings/vim/plugin
-cp -r {path_to_powerline}/bindings/vim/plugin ~/.vim/
 
-# vundle
+# vim
+## vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# pathogen
+## pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# plugins
+## plugins
 vim +PluginInstall +qall
 
-# templates
+## templates
 cd ~/.vim/bundle
 if [ ! -d "vim-template" ]; then
     git clone git://github.com/aperezdc/vim-template.git
 fi
 
-# YouCompleteMe
+## YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe
 # ./install.py --clang-completer
 ./install.py
